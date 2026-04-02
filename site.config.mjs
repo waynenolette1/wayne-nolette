243db5386a3214694f44e42dbf77b3e492cad9af
waynenolette1 <waynenolette@gmail.com>
2026-04-02 18:39:40 -0400

    feat: formats

--- site.config.mjs
+++ site.config.mjs
@@ -1,3 +1,3 @@
 /** Shared site config importable by ESM plugins (remark, rehype) and astro.config.mjs. */
 export const BASE_PATH = '/wayne-nolette/';
-export const SITE_URL = 'https://wayne-nolette.github.io/wayne-nolette';
+export const SITE_URL = 'https://waynenolette1.github.io/wayne-nolette';
